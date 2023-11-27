89c86ecc82d4749d72c1833bf7120f929a4d10a1
Huy Tuan <tuanthhe172693@fpt.edu.vn>
2023-11-28 02:01:36 +0700

    fix some errors

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -49,7 +49,11 @@ function NavBar() {
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="ms-auto" defaultActiveKey="#home">
             <Nav.Item>
-              <Nav.Link as={Link} to="/" onClick={() => updateExpanded(false)}>
+              <Nav.Link
+                as={Link}
+                to="/Portfolio"
+                onClick={() => updateExpanded(false)}
+              >
                 <AiOutlineHome style={{ marginBottom: "2px" }} /> Home
               </Nav.Link>
             </Nav.Item>
